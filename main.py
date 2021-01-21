9390c163fc87b154ebb34ac8b35b5f170ec04ec2
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-21 05:40:23 +0000

    Add user prompts to submit names

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ import random
 # Set a random seed
 num_people = int(input("How many people are in the group? \n"))
 random.seed(num_people)
+
+# Split string method
+name_submission = input("Submit everybody's name, separated by a comma and space e.g. 'John, Anna'. \n").title()
+names = name_submission.split(", ")
+
